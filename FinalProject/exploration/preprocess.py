280c2368a09c8c6a07d78f61a75daf67537aec6b
jerrymhuang <aca.jerryh@gmail.com>
2024-04-19 11:55:27 -0400

    Simplified and reformatted dataset for Final Project

--- FinalProject/exploration/preprocess.py
+++ FinalProject/exploration/preprocess.py
@@ -2,9 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 
+def convert():
+    files = os.listdir("../data/subjects_xy/")
+    return files
 
 def main():
     pass
 
 if __name__ == "__main__":
-    main()
+    files = convert()
